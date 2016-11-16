6315a654a3a7882de3bcc65e36a4747b1c86bcee
Herro-Sama <sandylily66@hotmail.com>
2016-11-16 11:17:51 +0000

    Adding all Comp120 workshop activities for further referencing.

---
+++ AltCode.py
@@ -0,0 +1,19 @@
+import wave
+import struct
+import random
+
+noise_output = wave.open('noise2.wav', 'w')
+noise_output.setparams((2, 2, 44100, 0, 'NONE', 'not compressed'))
+
+values = []
+
+for i in range(0, SAMPLE_LEN):
+        value = random.randint(-32767, 32767)
+        packed_value = struct.pack('h', value)
+        values.append(packed_value)
+        values.append(packed_value)
+
+value_str = ''.join(values)
+noise_output.writeframes(value_str)
+
+noise_output.close()
