7f40ce548dfe1bf1218e04c3643b089e27ed3e5c
Alex <sandylily66@hotmail.com>
2016-11-30 12:00:33 +0000

    Updated Melody Generator.py almost working.

--- Melody Generator.py
+++ Melody Generator.py
@@ -2,8 +2,8 @@ import wave
 import struct
 import math
 
-amplitude = 0.2
-BitDepth = 64
+amplitude = 0.4
+BitDepth = 65000
 
 Sound = wave.open('noise2.wav','r')
 Soundtuple = Sound.getparams()
@@ -12,20 +12,22 @@ for i in range(0, Soundtuple[3]):
     Soundinfo = Sound.readframes(1)
     Soundstruct = struct.unpack("<h",Soundinfo)
 
+Soundtuple = Sound.getparams()
+
 Sound2 = wave.open('keyboard.wav','r')
-Soundtuple2 = Sound.getparams()
+Soundtuple2 = Sound2.getparams()
 
 for x in range(0, Soundtuple2[3]):
     Sound2info = Sound2.readframes(1)
     Sound2struct = struct.unpack("<h",Sound2info)
 
-noise_out_tuple = (1, 2, Soundtuple[2] + Soundtuple2[2], Soundtuple[3] + Soundtuple2[3], 'NONE', 'Not compressed')
+noise_out_tuple = (1, 2, 44100, 176400,'NONE', 'Not compressed')
 noise_out = wave.open('Melody.wav', 'w')
 noise_out.setparams(noise_out_tuple)
 
 values = []
 for y in range(0, noise_out_tuple[3]):
-    value = math.sin(2.0 * math.pi * (y / Soundtuple[2] + Soundtuple2[2])) * (amplitude * BitDepth)
+    value = math.sin(2.0 * math.pi * (y / noise_out_tuple[2])) * (amplitude * BitDepth)
     print value
     packaged_value = struct.pack('h', value)
 
@@ -39,4 +41,7 @@ print noise_out_tuple[3]
 print noise_out_tuple[4]
 print noise_out_tuple[5]
 
+print Soundtuple
+print Soundtuple2
+noise_out.writeframes(values)
 noise_out.close()
