e1ffabb602f3502174011b755785bad27a3f7532
Alex Mitchell <sandylily66@hotmail.com>
2016-11-30 19:21:09 +0000

    Attempted to make a file work, will try again after another attempt.

--- Melody Generator.py
+++ Melody Generator.py
@@ -5,7 +5,7 @@ import math
 amplitude = 0.4
 BitDepth = 65000
 
-Sound = wave.open('noise2.wav','r')
+Sound = wave.open('mellow.wav','r')
 Soundtuple = Sound.getparams()
 
 for i in range(0, Soundtuple[3]):
@@ -14,12 +14,12 @@ for i in range(0, Soundtuple[3]):
 
 Soundtuple = Sound.getparams()
 
-Sound2 = wave.open('keyboard.wav','r')
+Sound2 = wave.open('mellow2.wav','r')
 Soundtuple2 = Sound2.getparams()
 
 for x in range(0, Soundtuple2[3]):
     Sound2info = Sound2.readframes(1)
-    Sound2struct = struct.unpack("<h",Sound2info)
+    Sound2struct = struct.unpack("<h", Sound2info)
 
 noise_out_tuple = (1, 2, 44100, 176400,'NONE', 'Not compressed')
 noise_out = wave.open('Melody.wav', 'w')
@@ -33,13 +33,6 @@ for y in range(0, noise_out_tuple[3]):
 
     for j in xrange(0, noise_out_tuple[0]):
         values.append(packaged_value)
-
-print noise_out_tuple[0]
-print noise_out_tuple[1]
-print noise_out_tuple[2]
-print noise_out_tuple[3]
-print noise_out_tuple[4]
-print noise_out_tuple[5]
 
 print Soundtuple
 print Soundtuple2
